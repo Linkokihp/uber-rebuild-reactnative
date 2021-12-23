493c5c2574d87c2d27f2ecffd6b82ab2f52f03fe
Koch Philipp <Philipp.Koch@kilchenmann.ch>
2021-12-23 16:25:46 +0100

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
+import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
+import { store } from "./store";
+
+//1:Setting up Redux (done)
+//2: 
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+          <SafeAreaProvider>
+            <HomeScreen/>
+          </SafeAreaProvider>
+    </Provider>
   );
 }
 
---
+++ screens/MapScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+
+
+const MapScreen = () => {
+    return (
+        <View>
+            <Text>Here will be the map...</Text>
+        </View>
+    )
+}
+
+export default MapScreen
+
+const styles = StyleSheet.create({})
